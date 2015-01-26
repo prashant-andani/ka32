df7926011702678d47392b6e7159904fe968af7a
prashant andani <prashant.andani@careervita.com>
2015-01-26 11:06:58 +0530

    changing files

---
+++ careerguidance.php
@@ -0,0 +1,174 @@
+	<?php include("header.php");?>
+
+	<section class="features intro" id="intro">
+	
+		<div class="row">
+			<div class="container">
+				<div class="col-sm-2">
+    				<nav class="nav-sidebar">
+						<ul class="nav tabs">
+					        <li class="active"><a href="#career_guidance" data-toggle="tab"><i class="fa fa-university"></i>&nbsp;&nbsp;Career Guidance</a></li>
+					        <li class=""><a href="#resume_etiquettes" data-toggle="tab"><i class="fa fa-file-text-o"></i>&nbsp;&nbsp;Resume Etiquettes</a></li>
+					        <li class=""><a href="#interview_tips" data-toggle="tab"><i class="fa fa-trophy"></i>&nbsp;&nbsp;Interview Tips</a></li>
+					       <!-- <li class=""><a href="#personality_development" data-toggle="tab"><i class="fa fa-bus"></i>&nbsp;&nbsp;Personality Development</a></li>
+					        <li class=""><a href="#communication_skills" data-toggle="tab"><i class="fa fa-car"></i>&nbsp;&nbsp;Communication Skills</a></li>
+					        <li class=""><a href="#test_your_skills" data-toggle="tab"><i class="fa fa-puzzle-piece"></i>&nbsp;&nbsp; Test your Aptitude Skills</a></li> 
+					        --> 
+							                          
+						</ul>
+					</nav>
+				</div>
+			<!-- tab content -->
+			<div class="tab-content">
+				<div class="tab-pane active text-style" id="career_guidance">
+					<h2><i class="fa fa-university"></i> Career Guidance</h2>
+					<br/>
+					<p> Career Guidance is a comprehensive, human development program designed to assist individuals and clarify career goals. With Career Guidance counselling, it helps you to understand your strengths, skills and guide you the career paths, learning opportunities, educational programmes, the labour market trends and conditions, training institutions and Job Opportunities in various sectors.
+					This page brings you the industry standards videos on Career Guidance. 
+					
+					</p>
+					
+					<p><b>Introduction to Engineering : </b>  Check out these videos here to get a introductory walkthrough on every Engineering Course before you decide the course you want to join in !!!
+					</p>
+						<object width="150" height="120" data="http://www.youtube.com/v/DL_Ba01-idU"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/yTcMuzvPb-s"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/HkkSKuRkYrc"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/a8ycSFd7f0c"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/BwozLyzg52g"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/O7atONXpBeA"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/oeIC0dQmqm0"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/TIjd7ZZ7Vlk"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/ldSTXHXCgAE"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/uTQaZ-jaNIs"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/FMptS53hE30"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/decyiTmWDOI"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/nRAg1Pnt6Ag"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/6h1HKkrh_Rk"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/0VumQUDlUSA"></object>
+					
+					<p><b>Career Guidance Materials </b> : Are you facing difficulties in choosing your career ? Are you confused which career path to choose for your bright future ? No Worries !! Explore some of the most acclaimed video materials which helps you to grow.
+					</p>
+						<object width="150" height="120" data="http://www.youtube.com/v/fPicK-RPaO8"></object>	
+						<object width="150" height="120" data="http://www.youtube.com/v/-0KuxYZvSqg"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/5qF1CtQe__U"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/MBkThTRCB74"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/IZVN-yuXPos"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/KJyTwPR_hbU"></object>
+						
+					<p><b> Is Engineering right for you ? Why Study Engineering ?</b>
+					</p>					
+						<object width="150" height="120" data="http://www.youtube.com/v/vj-H_Mbfvu4"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/zoHm5AXeYYQ"></object>
+
+					<p><b>Women in the world of Engineering </b> : Learn the scope and opportunities for Women in Engineering </p>
+						
+						<object width="150" height="120" data="http://www.youtube.com/v/5mtfPS9z_pw"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/xA0MVnOiZNk"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/9kxiHAhSL-w"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/XgQZA6zvfIU"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/aH8UcVb5PKw"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/aH8UcVb5PKw"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/0zr0RAi6hZM"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/A18nG6Sh8hs"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/ug2KaBBy6Rc"></object>
+
+				</div>
+				
+				<div class="tab-pane text-style" id="resume_etiquettes">
+					<h2><i class="fa fa-file-text-o"></i> Resume Etiquettes</h2>
+					<p>
+					</p>
+
+				</div>
+				
+				<div class="tab-pane text-style" id="interview_tips">
+					<h2><i class="fa fa-trophy"></i> Interview Tips</h2>
+					<p><b>Interview</b> : An interview is a conversation between two or more people where questions are asked by the interviewer to elicit facts or statements from the interviewee. Additionally, it is a procedure designed to obtain information from a person's oral response to oral inquiries. 
+					<br>Many Job seeksers often struggle to face the interview because of low confidence level, afraid, lack of knowledge, poor physical appearances etc. No more worries !! In this portal, we bring you the tips and the etiquettes to face professional Interviews.<hr>
+					<br><b> Interview Tips </b>:  check out these videos on a basic tips to face interviews
+					</p>
+				   		<object width="150" height="120" data="http://www.youtube.com/v/qKBubKO-798"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/qSylCmes5dw"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/OW-yxxPMtro"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/u_VdMHGQnZU"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/UXwW6I3-bUA"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/g8Hk19u89A0"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/78EtYVKJjpw"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/KLdboEjNSdM"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/A95xT0GQj7A"></object>
+					<hr>
+					<p><b> Successful Interview </b> : check out these videos for a mock-up interviews	</p> 
+						<object width="150" height="120" data="http://www.youtube.com/v/xkPTJwSWnJ8"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/1mHjMNZZvFo"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/ppHDuTJe39g"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/KFeGt_vHd1k"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/S1ucmfPOBV8"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/epcc9X1aS7o"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/WRsYTMWaKhE"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/upe24j0zju8"></object>
+						<object width="150" height="120" data="http://www.youtube.com/v/0p_A2P_uvzc"></object>
+				</div>
+			
+				<div class="tab-pane text-style" id="test_your_skills">
+					<h2> <i class="fa fa-puzzle-piece"></i>&nbsp;Aptitude Skills</h2>
+					<p>Aptitudes are natural talents, special abilities for doing, or learning to do, certain kinds of things easily and quickly. They have little to do with knowledge or culture, or education, or even interests. 
+						<br>Aptitude Skill tests are time based designed to measure individual work-related cognitive capacity. Apititude Skills test measure the inidividuals intelligence : 
+						<li>ability to think and reason abstractly and solve problems</li>
+						<li>ability to learn from past experiences and relevant learning, and to apply this learning to a situation </li>
+						<br>
+						<b>Types of Aptitude Tests :</b>
+						<li>Abstract reasoning :Also called conceptual reasoning, this test is unique in its design. It is a non-verbal test, which uses shapes rather than words or text to measure someone’s fluid intelligence. Each test question includes a series of shapes with common logical rules.</li>
+						<li>Verbal reasoning test : This is a timed test that is designed to measure your verbal analytical skills (or verbal reasoning skills). These skills include the capacity to quickly identify critical issues from written material such as reports, and logically derive conclusions from written facts or data</li>
+						<li>Numerical Reasoning Test : This test is also a timed test. It measures your numerical analytical skills (or numerical reasoning skills).These skills include the capacity to quickly identify critical issues from numerical data such as graphs and tables. It also includes the capacity to use work-related numerical data such as performance figures or financial outcomes to make effective decisions.</li>
+					
+						<br>These Aptitude skill tests are of high importance for Job seekers after the graduation ranging from Engineering, Law, Banking and Accounting, Civil Service / Departmental Examinations and pursuing higher studies. Learn the aptitude skills in problem solving through this classroom based video.
+					</p>
+						<object width="150" height="120" data="https://www.youtube.com/v/0Hqd1M0UuAQ&list=PLS3LRWNnicw93cogvY91oO0mVXb5cNJ3R"></object>
+										
+					<p><b>Test your aptitude skills now !!</b>
+					<li><a href="http://www.psychometricinstitute.com.au/">Aptitude Skill Test1 </a></li>
+					<li><a href="http://www.practiceaptitudetests.com">Aptitude Skill Test2 </a></li>
+					<li><a href="http://www.freshersworld.com/">Aptitude Skill Test3 </a></li>
+					<li><a href="http://www.pskills.org/index.jsp">Aptitude Skill Test4 </a></li>
+					<li><a href="http://www.jobtestprep.co.uk/">Aptitude Skill Test5 </a></li>
+					<li><a href="http://www.fresherslive.com/">Aptitude Skill Test6 </a></li>
+					<li><a href="http://exam2win.com/online-tests/aptitude-test/">Aptitude Skill Test7 </a></li>
+					<li><a href="http://www.youth4work.com/">Aptitude Skill Test8 </a></li>
+					</p>
+
+					
+					
+				</div>
+				<!--
+				<div class="tab-pane text-style" id="personality_development">
+					<h2> <i class="fa fa-bus"></i> Personality Development</h2>
+					<p>
+					</p>
+					
+				</div>
+
+				<div class="tab-pane text-style" id="communication_skills">
+					<h2><i class="fa fa-car"></i> Communication Skills</h2>
+					<p>
+						
+					</p>
+
+				</div>-->
+
+			
+			
+		</div>
+    
+	</div>
+
+
+	
+	</div>
+
+	<span class="mouse"></span>
+	
+</section>
+<!-- end intro section -->
+
+
+<?php include("footer.php");?>
